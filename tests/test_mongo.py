3b2750e1af866984dbbc3f9e88f6be0b3e031280
Alejandro Ricoveri <alejandro.ricoveri@blanclink.com>
2014-10-25 17:14:07 -0430

    mongo module basic tests

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -2,23 +2,42 @@
 Test for: mongo
 """
 
-from nose.tools import assert_equals
-from m2s3 import app, config, const
+from nose.tools import raises, eq_, ok_
+from m2s3 import mongo
+import os
+
+
+@raises(TypeError)
+def test_make_tarfile_nonstr():
+    # _make_tarfile with non-string src_dir
+    mongo._make_tarfile(123)
+
 
 def test_make_tarfile():
-    # _make_tarfile with invalid directory
-    # _make_tarfile with non-string src_dir
-    pass
+    # whether the file name return is the expected
+    out_dir = "../data"
+    out_file = out_dir + '.tar.gz'
+    eq_(mongo._make_tarfile(out_dir), out_file)
+    # whether the expected file exists
+    ok_(open(out_file))
+    os.remove(out_file)
 
 
-def make_backup_file():
-    # make_backup_file with valid data
+@raises(TypeError)
+def test_make_backup_file_nondict():
     # make_backup_file with non-dict data
+    mongo.make_backup_file(123)
+
+
+@raises(TypeError)
+def test_make_backup_file_invalid():
     # make_backup_file with invalid values within data
     # (for example, when type(data['mongodump']) == int)
-    # with either empty user_name or password
-    # with either invalid or empty dbs, it has to be an array in the first place
-    pass
+    mongo.make_backup_file({'mongodump': 123})
 
-def test_mongodump():
-    pass
+
+@raises(ValueError)
+def test_make_backup_file_empty_db():
+    # with either invalid or empty dbs, it has to be an array in the first place
+    mongo.make_backup_file({'db': []})
+
