8e8cefcde52515f04e070a1cb49c56ea5bb9f2cc
Alejandro Ricoveri <alejandro.ricoveri@blanclink.com>
2014-10-26 13:07:43 -0430

    Tests setup for 's3' module

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -2,5 +2,17 @@
 Test for: s3
 """
 
-from nose.tools import assert_equals
-from m2s3 import app, config, const
+from nose.tools import eq_, ok_, raises
+from m2s3 import s3, config
+
+def setup():
+    config.clear()
+
+@raises(TypeError)
+def test_backup_file_nonstr():
+    s3.backup_file(123)
+
+@raises(ValueError)
+def test_backup_file_incomplete_aws():
+    config.set_entry('aws', {'id' : 123})
+    s3.backup_file('file.txt')
