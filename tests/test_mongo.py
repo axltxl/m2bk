4be17dcb45b5d0bd1abe89c6a0581f6aa5895139
Alejandro Ricoveri <alejandro.ricoveri@blanclink.com>
2014-10-30 15:15:45 -0430

    One config to rule them all ...
    One config file can handle several hosts and their databases, a whole new
    way to configure the mongodb section is available, which is more flexible and
    versatile, details are included in README.md.

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -24,32 +24,96 @@ def test_make_tarfile():
     os.remove(out_file)
 
 
-def test_make_backup_file_invalid_kwargs():
+def test_set_mongodb_host_val():
+    """
+    Test whether mongo._set_mongodb_host_val works well
+    """
+    last_resort = "mate"
+    defaults = {
+        'hello': 'world',
+        'marco': 'polo'
+    }
+    values = {
+        'marco': 'what?'
+    }
+    mongo._set_mongodb_host_val('marco', 'not_good', values, defaults)
+    eq_(values['marco'], 'what?')
+
+    mongo._set_mongodb_host_val('hello', 'not_good', values, defaults)
+    eq_(values['hello'], 'world')
+
+    mongo._set_mongodb_host_val('ello', last_resort, values, defaults)
+    eq_(values['ello'], last_resort)
+
+
+def _test_make_backup_files(e, **kwargs):
+    """
+    Individual test for mongo.make_backup_files
+    """
+    hosts = [
+        {
+            'name': 'ello',
+            'address' : 'somehost',
+            'dbs': ['marco', 'polo']
+        },
+        {
+            'name': 'ello_again',
+            'address': 'somehost',
+            'dbs': ['marco', 'polo']
+        }
+    ]
+    assert_raises(e, mongo.make_backup_files, dry_run=True, hosts=hosts, **kwargs)
+
+
+def test_merge_dbs():
+    a = ['hello', 'konichiwa']
+    b = ['salam', 'hello', 'bonjour']
+    # a + b = c (it should be)
+    c = ['hello', 'salam', 'bonjour', 'konichiwa']
+    eq_(sorted(c), sorted(mongo._merge_dbs(a,b)))
+
+def test_make_backup_files_invalid_kwargs():
+    """
+    Several tests for mongo.make_backup_files
+    """
+    # hosts must be a list
+    assert_raises(TypeError, mongo.make_backup_files, hosts=123)
+    assert_raises(ValueError, mongo.make_backup_files, hosts=[])
     # mongodump must be str
-    assert_raises(TypeError, mongo.make_backup_file, mongodump=123)
-    assert_raises(ValueError, mongo.make_backup_file, mongodump='')
+    _test_make_backup_files(TypeError, mongodump=123)
+    _test_make_backup_files(ValueError, mongodump='')
     # output_dir must be str
-    assert_raises(TypeError, mongo.make_backup_file, output_dir=123)
-    assert_raises(ValueError, mongo.make_backup_file, output_dir='')
-    # host must be str
-    assert_raises(TypeError, mongo.make_backup_file, host=123)
-    assert_raises(ValueError, mongo.make_backup_file, host='')
+    _test_make_backup_files(TypeError, output_dir=123)
+    _test_make_backup_files(ValueError, output_dir='')
     # port must be int
-    assert_raises(TypeError, mongo.make_backup_file, port="123")
+    _test_make_backup_files(TypeError, host_defaults={"port": "123"})
     # port must be between 1 and 65535
-    assert_raises(ValueError, mongo.make_backup_file, port=-40)
-    assert_raises(ValueError, mongo.make_backup_file, port=340535)
+    _test_make_backup_files(ValueError, host_defaults={"port": -40})
+    _test_make_backup_files(ValueError, host_defaults={"port": 340535})
     # user_name must be str
-    assert_raises(TypeError, mongo.make_backup_file, user_name=123)
-    assert_raises(ValueError, mongo.make_backup_file, user_name='')
+    _test_make_backup_files(TypeError, host_defaults={"user_name": 123})
+    _test_make_backup_files(ValueError, host_defaults={"user_name": ''})
     # password must be str
-    assert_raises(TypeError, mongo.make_backup_file, password=123)
-    assert_raises(ValueError, mongo.make_backup_file, password='')
+    _test_make_backup_files(TypeError, host_defaults={"password": 123})
+    _test_make_backup_files(ValueError, host_defaults={"password": ''})
+    # name must be str
+    assert_raises(TypeError, mongo.make_backup_files, hosts=[{'name': 123}])
+    assert_raises(ValueError, mongo.make_backup_files, hosts=[{'name': ''}])
 
-
-def test_make_backup_file_invalid_dbs():
     # with either invalid or empty dbs, it has to be an array in the first place
-    assert_raises(ValueError, mongo.make_backup_file, dbs=[])
-    assert_raises(ValueError, mongo.make_backup_file, dbs=123)
-    assert_raises(TypeError, mongo.make_backup_file, dbs=['asd', 123])
+    hosts = [
+        {
+            'name': 'ello',
+            'address' : 'somehost',
+            'dbs': []
+        },
+    ]
+    assert_raises(ValueError, mongo.make_backup_files, hosts=hosts)
+
+    hosts[0]["dbs"] = 123
+    assert_raises(ValueError, mongo.make_backup_files, hosts=hosts)
+
+
+    hosts[0]["dbs"] = ['asd', 123]
+    assert_raises(TypeError, mongo.make_backup_files, hosts=hosts)
 
