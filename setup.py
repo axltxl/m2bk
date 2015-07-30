09dde61c1bee097d3e4bf4096660bc07534bdd7b
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-07-30 08:15:46 -0430

    Initial support for upcoming python3.5

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.3',
     ],
