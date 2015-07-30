41e94e18a9eba8d006f903cffb6fb6ab354af9c3
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-07-30 15:21:29 -0430

    Several corrections on log module

--- m2bk/log.py
+++ m2bk/log.py
@@ -15,13 +15,16 @@ System-wide messages are logged through logging.
 
 import sys
 import logging
+import logging.handlers
 #from clint.textui import colored
 from .const import PKG_NAME
+from .utils import debug
 
 from clint.textui.colored import white, red, cyan, yellow, green
 
 # Globals
 _logger = None
+_log_lvl = 0
 _stdout = False
 
 def _set_lvl(lvl):
@@ -46,17 +49,18 @@ def init(threshold_lvl, quiet_stdout):
     :param threshold_lvl: messages under this level won't be issued/logged
     :param to_stdout: activate stdout log stream
     """
-    global _logger
+    global _logger, _log_lvl
 
     # translate lvl to those used by 'logging' module
-    log_lvl = _set_lvl(threshold_lvl)
+    _log_lvl = _set_lvl(threshold_lvl)
 
     # logger Creation
     _logger = logging.getLogger(PKG_NAME)
-    _logger.setLevel(log_lvl)
+    _logger.setLevel(_log_lvl)
 
     # create syslog handler and set level to info
     syslog_h = logging.handlers.SysLogHandler(address='/dev/log')
+    #log_h = logging.FileHandler('/var/log/m2bk.log') # for later
 
     # Base message format
     base_fmt = '%(name)s - [%(levelname)s] - %(message)s'
@@ -116,6 +120,7 @@ def msg_debug(message):
 
     :param message: the message to be logged
     """
-    to_stdout(message, colorf=cyan)
-    if _logger:
-        _logger.debug(message)
+    if _log_lvl == logging.DEBUG:
+        to_stdout(message, colorf=cyan)
+        if _logger:
+            _logger.debug(message)
